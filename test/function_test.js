38506780f7358380dadbb880ac0b3271283a1b4c
Howard Yeh <howard@metacircus.com>
2016-08-01 15:40:02 +0800

    function exercises

---
+++ test/function_test.js
@@ -0,0 +1,24 @@
+const { assert } = require("chai");
+const fun = require("../src/function");
+
+describe("function", () => {
+  describe("函数为值", () => {
+    it("数组里面有三个函数，调用分别返回 1，2，3", () => {
+      const [one, two, three] = fun.arrayOfFunctions();
+
+      assert.equal(one(), 1);
+      assert.equal(two(), 2);
+      assert.equal(three(), 3);
+    });
+
+    it("字典里面有三个函数，调用分别返回 a，b，c", () => {
+      const fns = fun.dictionaryOfFunctions();
+
+      assert.equal(fns["a"](), "a");
+      assert.equal(fns["b"](), "b");
+      assert.equal(fns["c"](), "c");
+    });
+
+
+  });
+});
